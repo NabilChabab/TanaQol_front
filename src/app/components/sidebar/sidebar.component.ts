eb5a8115760f79d54a81d40c026223a9f2927706
NabilChabab <nabil.chababnabil@gmail.com>
2025-02-18 11:08:42 +0100

    role request

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
+import { AuthService } from "../../core/services/auth/auth.service";
 
 @Component({
   selector: "app-sidebar",
@@ -10,8 +11,13 @@ import { RouterModule } from "@angular/router";
 })
 export class SidebarComponent implements OnInit {
   collapseShow = "hidden";
-  constructor() {}
+  constructor(public authService : AuthService , public router : Router) {}
 
   ngOnInit() {}
- 
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+
 }
