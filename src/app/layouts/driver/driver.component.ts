4b62de2012632a9cd1b3662b7ef4305610894dce
NabilChabab <nabil.chababnabil@gmail.com>
2025-02-18 11:10:25 +0100

    role request

---
+++ src/app/layouts/driver/driver.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { AdminNavbarComponent } from "../../components/navbars/admin-navbar/admin-navbar.component";
+import { HeaderStatsComponent } from "../../components/headers/header-stats/header-stats.component";
+import { FooterAdminComponent } from "../../components/footers/footer-admin/footer-admin.component";
+
+@Component({
+  selector: "app-admin",
+  standalone : true,
+  templateUrl: "./driver.component.html",
+  imports: [RouterModule ,SidebarComponent  , AdminNavbarComponent , HeaderStatsComponent , FooterAdminComponent  ],
+})
+export class DriverComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
