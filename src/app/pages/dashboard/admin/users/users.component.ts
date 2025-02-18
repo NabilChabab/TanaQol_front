9d0d23f96a42b3514c11de74fbc2f7e04fa45812
NabilChabab <nabil.chababnabil@gmail.com>
2025-02-18 11:11:04 +0100

    role request

--- src/app/pages/dashboard/admin/users/users.component.ts
+++ src/app/pages/dashboard/admin/users/users.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth/auth.service';
+import { CardUsersComponent } from '../../../../components/cards/card-users/card-users.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule , RouterModule],
+  imports: [CommonModule , RouterModule , CardUsersComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
